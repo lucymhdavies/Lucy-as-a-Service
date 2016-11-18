8b85d907e918c9350328b8c53a4d37860f473735
Lucy Davies <lucy.davies@skybettingandgaming.com>
2016-11-18 13:06:48 +0000

    Revert "Attempt to impersonate the user who asked to replay the message"
    This reverts commit d3661bdd377609b50f0ed5696d950824f75fc7b4.

--- cmd/standup.rb
+++ cmd/standup.rb
@@ -200,7 +200,7 @@ def standup_next
 			warn "And she has a saved message!"
 			task = Thread.new {
 				post_data = replay_message p['user_id'], :standup_next
-				sleep(2)
+				sleep(0.1)
 				RestClient.post(params['response_url'], post_data )
 			}
 		else
