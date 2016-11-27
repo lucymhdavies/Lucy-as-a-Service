96b1b01e5e33eaf7b92cd43f72ade435ba9b4154
Lucy Davies <lucy@lucymhdavies.com>
2016-11-27 22:35:54 +0000

    Added conditional monkeying around

--- cmd/monkey.rb
+++ cmd/monkey.rb
@@ -2,8 +2,12 @@
 # TODO
 
 def monkey
+	unless can_use_monkey?
+		return slack_secret_message "Unable to use Monkey commands in this Slack team. Wait for OAuth"
+	end
+
 	case params['text'].chomp
-	when "monkey", "monkey who"
+	when "monkey", "monkey who", "monkeys", "monkeys who"
 		slack_secret_message "TODO: List current monkeys. Exclude ps-user"
 		# https://api.slack.com/methods/usergroups.list (get group ID, and check if group is disabled)
 		# https://api.slack.com/methods/usergroups.users.list
@@ -19,4 +23,8 @@ def monkey
 	else
 		slack_secret_message "I don't know what to do with: #{params['text'].chomp}"
 	end
+end
+
+def can_use_monkey?
+	false
 end
