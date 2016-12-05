b0fd2f4fbff0ffe2976fb195c7946bdeb86955dc
Lucy Davies <lucy@lucymhdavies.com>
2016-12-05 22:08:22 +0000

    Saved Message now stored in Redis

--- cmd/saved_message.rb
+++ cmd/saved_message.rb
@@ -1,30 +1,32 @@
 
-# TODO: store this in a db of some sort
-$user_vars = {}
 def save_message
 	saved_message_text = params['text'].sub(/save */, "")
 	saved_message_text = slack_parse saved_message_text
 
 
-	$user_vars[params['user_id']] = { :saved_message => saved_message_text }
+	$redis.setex( "laas:saved_message:#{params['user_id']}", 60 * 30, saved_message_text )
 
-	"Insecurely Saved:\n\n" + $user_vars[params['user_id']][:saved_message]
+	"Insecurely Saved:\n\n" + saved_message_text
 end
 
 def replay_message
-	if $user_vars[params['user_id']] && $user_vars[params['user_id']][:saved_message]
-		# if invoked with /laas replay standup.*
-		# trigger /laas standup next
-		if params['text'].start_with?("replay standup")
-			task = Thread.new {
-				sleep(2)
-				post_data = standup_next
-				RestClient.post(params['response_url'], post_data )
-			}
-		end
+	r = $redis.get( "laas:saved_message:#{params['user_id']}" )
 
-		slack_message $user_vars[params['user_id']][:saved_message]
-	else
-		slack_secret_message "No saved message for you"
+	if r.nil? || r == ""
+		return slack_secret_message "No saved message for you"
 	end
+
+	message = r
+
+	# if invoked with /laas replay standup.*
+	# trigger /laas standup next
+	if params['text'].start_with?("replay standup")
+		task = Thread.new {
+			sleep(2)
+			post_data = standup_next
+			RestClient.post(params['response_url'], post_data )
+		}
+	end
+
+	slack_message message
 end
